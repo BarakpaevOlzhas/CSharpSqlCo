83099a6850f3a7a1c7862e7c8bbf0a77ba222614
BarakpaevOlzhas <40425093+BarakpaevOlzhas@users.noreply.github.com>
2018-11-18 20:49:31 +0600

    Add files via upload

---
+++ SqlDz/SqlDz/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace SqlDz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection())
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings["UserConectionString"].ConnectionString;
+                sqlConnection.ConnectionString = connectionString;
+
+                sqlConnection.Open();
+
+                SqlCommand sqlCommand = new SqlCommand();
+
+                sqlCommand.Connection = sqlConnection;
+
+                sqlCommand.CommandText = "Create Table Gruppa (id int not null, Name Nvarchar(MAX))";
+
+                SqlDataReader reader = sqlCommand.ExecuteReader();
+            }
+        }
+    }
+}
